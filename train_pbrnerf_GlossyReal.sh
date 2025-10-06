c3167693abbf3e86a087d76716164fb824788b96
alirezaisavand <isavand.alireza@gmail.com>
2025-10-06 17:29:24 -0600

    update bash and config for training on GlossyReal

---
+++ train_pbrnerf_GlossyReal.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# Set the default scene
+SCENE="bear_raw1024"
+TAGS="debug"
+CONS_LOSS=0.0
+SPEC_LOSS=0.0
+
+# Override with the first command-line argument, if provided
+if [ -n "$1" ]; then
+  SCENE="$1"
+fi
+
+if [ -n "$2" ]; then
+  TAGS="$2"
+fi
+
+if [ -n "$3" ]; then
+  CONS_LOSS="$3"
+fi
+
+if [ -n "$4" ]; then
+  SPEC_LOSS="$4"
+fi
+
+echo "Using scene: $SCENE"
+
+cd code
+OPENCV_IO_ENABLE_OPENEXR=1 python3 training/train.py \
+  /workspace/datasets/GlossyReal/$SCENE \
+  outputs \
+  --name pbrnerf_GlossySynthetic_$SCENE \
+  --tags $TAGS \
+  --override_cons_weighting $CONS_LOSS \
+  --override_spec_weighting $SPEC_LOSS \
+  --config_path configs/config_GlossySynthetic_pbrnerf.json
+
